001b6af21af60f754f74c9bcc7daa217133a81c6
yeqianmen <56306966+yeqianmen@users.noreply.github.com>
2024-05-23 13:26:48 +0800

    fix camel case

--- cmd/json.go
+++ cmd/json.go
@@ -2,29 +2,29 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func res_string(c *gin.Context) {
+func resString(c *gin.Context) {
 	c.String(200, "ok")
 
 }
-func res_json(c *gin.Context) {
-	type userjoson struct {
+func resJson(c *gin.Context) {
+	type UserJson struct {
 		Name     string `json:"name"`
 		Id       int
 		Age      int
 		Password string `json:"password"`
 	}
-	jsonUser := userjoson{
+	JsonUser := UserJson{
 		Name:     "wang",
 		Id:       1,
 		Age:      18,
 		Password: "qwer",
 	}
-	c.JSON(200, jsonUser)
+	c.JSON(200, JsonUser)
 	c.JSON(200, gin.H{"username": "Alice", "age": 18})
 }
 func main() {
 	r := gin.Default()
-	r.GET("/text", res_string)
-	r.GET("/json", res_json)
+	r.GET("/text", resString)
+	r.GET("/json", resJson)
 	r.Run(":17000")
 }
