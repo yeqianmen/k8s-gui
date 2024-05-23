f52c07a6e505280fbf54d1a317eef58028b54211
yeqianmen <56306966+yeqianmen@users.noreply.github.com>
2024-05-23 13:19:23 +0800

    increase form-date parameters

--- cmd/request.go
+++ cmd/request.go
@@ -10,8 +10,24 @@ func query(c *gin.Context) {
 	fmt.Println(c.Query("user"))
 	fmt.Println(c.QueryArray("user"))
 }
+func form(c *gin.Context) {
+	name := c.PostForm("name")
+	fmt.Println(name)
+	s := c.PostFormArray("name")
+	fmt.Printf("s: %v\n", s)
+	c.JSON(200, gin.H{
+		"nameArry": s,
+		"name":     name,
+	})
+	city := c.DefaultPostForm("city", "JiNan")
+	c.JSON(200, gin.H{
+		"city": city,
+	})
+	fmt.Println(c.MultipartForm())
+}
 func main() {
 	r := gin.Default()
 	r.GET("/query/:userId/:bookId", query)
+	r.POST("/list", form)
 	r.Run(":8080")
 }
